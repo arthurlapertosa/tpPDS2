ac69cce715af50e8cb8f71ee40071dc27063ffb5
André Luís Alves Rezende <dedeluis1999@gmail.com>
2019-06-11 14:42:04 -0300

    Mudanças nos Comentários
    Comentários de Freqüência Invertida

--- main/frequencia_invertida.cpp
+++ main/frequencia_invertida.cpp
@@ -6,13 +6,13 @@ frequencia_invertida::frequencia_invertida()
 
 void frequencia_invertida::inserir(map<string, map<string, int>> frequencia_palavra_, int num_docs_total)
 {
-for (auto i = frequencia_palavra_.begin(); i != frequencia_palavra_.end(); i++) {
+for (auto i = frequencia_palavra_.begin(); i != frequencia_palavra_.end(); i++) {//percorre o map frequencia palavra e adiciona os 
 	string palavra = i->first;
 	double frequencia_int;
 	double num_docs = num_docs_total;
 	double quantidade_docs_palavra_aparece = frequencia_palavra_[palavra].size();
-	frequencia_int = log10((num_docs / quantidade_docs_palavra_aparece));
-	frequencia_invertida_palavra_.insert(std::pair<string, double>(palavra, frequencia_int));//Adiciona o elemento ao mapa frequencia_total_palavra_
+	frequencia_int = log10((num_docs / quantidade_docs_palavra_aparece));// calcula a frequencia invertida de cada palavra
+	frequencia_invertida_palavra_.insert(std::pair<string, double>(palavra, frequencia_int));//Adiciona o elemento ao mapa frequencia_invertida_palavra_
 	}
 }
 
@@ -20,8 +20,8 @@ for (auto i = frequencia_palavra_.begin(); i != frequencia_palavra_.end(); i++) 
 void frequencia_invertida::imprimi_Invertida()
 {
 	cout << endl;
-	for (auto i = frequencia_invertida_palavra_.begin(); i != frequencia_invertida_palavra_.end(); i++) {
-		cout << i->first << " => " << i->second << "   ";
+	for (auto i = frequencia_invertida_palavra_.begin(); i != frequencia_invertida_palavra_.end(); i++) {//for que percorre todo o map frequencia_invertida_palavra_
+		cout << i->first << " => " << i->second << "   ";//escreve na tela a frequencia invertida da posição atual
 		cout << endl;
 	}
 
@@ -29,7 +29,7 @@ void frequencia_invertida::imprimi_Invertida()
 
 double frequencia_invertida::frequencia_invertida_palavra(string palavra)
 {
-	return frequencia_invertida_palavra_[palavra];
+	return frequencia_invertida_palavra_[palavra];// Retornando a frquencia invertida da palavra em especifico
 }
 
 frequencia_invertida::~frequencia_invertida()
